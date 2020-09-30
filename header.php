3d8417372cde4cdb33a1af8cd337aaed02b04008
Aline Denizot <aline.denizot2@gmail.com>
2020-09-30 15:27:53 +0200

    header essai

---
+++ header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Titre de la page</title>
+  <link rel="stylesheet" href="style.css">
+  <script src="script.js"></script>
+</head>
+<body>
+  <header>
+
+    <div class="marge-before"></div>
+      
+      <nav class="menu">
+          <ul>
+              <li><a href=#>about me</a></li>
+              <li><a href=#>professional experience</a></li>
+              <li><a href=#>training & education</a></li>
+              <li><a href=#>skills</a></li>
+              <li><a href=#>hobbies</a></li>
+              <li><a href=#>contact</a></li>
+          </ul>
+      </nav>
+
+      <div class="image-bg"></div>
+
+      <div class="text">
+
+      </div>
+  </header>
+</body>
+</html>
