3d4761b6e8c09fe3d1e67dafd645a5a25b182eb5
Andrej Ilievski <mrandrej.ilievski@gmail.com>
2020-10-01 15:27:36 +0200

    Modified documents and used the structure. 90%

---
+++ header.php
@@ -0,0 +1,17 @@
+<div class="menu">
+
+  <header>
+      <nav>
+          <div class="marge-before"></div>
+          <ul>
+              <li><a href=#>about me</a></li>
+              <li><a href='experience.php'>professional experience</a></li>
+              <li><a href=#>training & education</a></li>
+              <li><a href=#>skills</a></li>
+              <li><a href=#>hobbies</a></li>
+              <li><a href='contact.php'>contact</a></li>
+          </ul>
+      </nav>
+  </header>
+
+</div>
