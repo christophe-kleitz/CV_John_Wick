379723fb0c2248a794c61e068c63e400c429bb88
Aline Denizot <aline.denizot2@gmail.com>
2020-10-03 16:06:36 +0200

    ajout template. ajout structure dossier medias et src.

---
+++ header.php
@@ -0,0 +1,21 @@
+<div class="menu">
+
+  <header>
+    <div class="logo">
+        <img src="medias/logo.png" alt="Logo officiel de John Wick"/>
+    </div>
+    <nav>
+          
+        <div class="marge-before"></div>
+          <ul>
+              <li><a href=#>professional experience</a></li>
+              <li><a href=#>training & education</a></li>
+              <li><a href=#>skills</a></li>
+              <li><a href=#>hobbies</a></li>
+              <li><a href=contact.php>contact</a></li>
+          </ul>
+    </nav>
+  </header>
+
+</div>
+
