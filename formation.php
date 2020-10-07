12d98ebec1f6cd7a6571207d93d168d9be55fb07
Andrej Ilievski <34969186+mr-andrej@users.noreply.github.com>
2020-10-07 16:42:50 +0200

    Update formation.php

--- formation.php
+++ formation.php
@@ -6,6 +6,7 @@
   <link rel="stylesheet" href="css/style_template.css">
   <link rel="stylesheet" href="css/style_formation.css">
   
+  <link rel="icon" type="image/png" href="images/favicon.ico"/>
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
   <link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&display=swap" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Poppins:wght@200&display=swap" rel="stylesheet">
