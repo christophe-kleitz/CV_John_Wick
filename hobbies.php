68c704f38376ed31843e4c4d23abf18655cb472e
Mikail Ozdemir <professional.mikailozdemir@gmail.com>
2020-10-07 10:08:41 +0200

    le fichier 7/oct

--- hobbies.php
+++ hobbies.php
@@ -3,7 +3,8 @@
 <head>
   <meta charset="utf-8">
   <title>Hobbies</title>
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="../src/css/style_template.css">
+  <link rel="stylesheet" href="../src/css/style_hobbies.css">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
   <link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&display=swap" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Poppins:wght@200&display=swap" rel="stylesheet">
@@ -18,8 +19,10 @@
         <!--Intégration du menu-->        
         <?php include("header.php"); ?>
 
-        <div class="image-bg"> <!--Intégrer ici une image de bg-->
-            <img src="johnbg.jpg" alt="Me and my dog"/>
+        <div class="image-bg"> 
+            
+            <!--Intégrer ici une image de bg avec <img src.... /> -->
+            <img class="imageg" src= "./medias/johnbg.jpg"/>
         </div>
 
     </div>
@@ -30,19 +33,21 @@
         <div>    
             <h1 class='hobbies'>My Hobbies</h1>
 
-            <img class='giffy' src="./gifhobbies/wickdog.gif" >   
+            <img class='giffy' src="./images/gifhobbies/wickdog.gif" >   
             <h2>My Dog</h2>
                 <p>My dog, my only friend almost more loyal than my caliber .45APC. I love it.</p>
   
-            <img class='giffy' src="./gifhobbies/johnasr.gif" >    
+            <img class='giffy' src="./images/gifhobbies/johnasr.gif" >    
             <h2>My Guns</h2>
                 <p>My weapons, I choose them as well as the entrée, my dishes and my dessert at the restaurant.</p>
         
-            <img class='giffy' src="./gifhobbies/johnpolocrosse.gif" >
+            <img class='giffy' src="./images/gifhobbies/johnpolocrosse.gif" >
             <h2>The Polo-Cross</h2>
                 <p>I know this is not common, however none of my hobbies are.</p>
-        </div>     
-        <!--Ne pas supprimer ce qui suit-->
+        </div>
+       
+
+        <!--Ne pas changer ce qui suit-->
 
     <div>
 
