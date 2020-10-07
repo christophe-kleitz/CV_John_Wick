740b94e22261806f62083ca488a0de1caadcc7b0
Mikail-Ozdemir <71105127+Mikail-Ozdemir@users.noreply.github.com>
2020-10-07 14:54:23 +0200

    MODIFIER A LA MAIN ZEBI

--- hobbies.php
+++ hobbies.php
@@ -3,8 +3,18 @@
 <head>
   <meta charset="utf-8">
   <title>Hobbies</title>
+<<<<<<< HEAD
+<<<<<<< HEAD
+  <link rel="stylesheet" href="css/style_template.css">
+  <link rel="stylesheet" href="css/style_hobbies.css">
+=======
   <link rel="stylesheet" href="../src/css/style_template.css">
   <link rel="stylesheet" href="../src/css/style_hobbies.css">
+>>>>>>> 68c704f38376ed31843e4c4d23abf18655cb472e
+=======
+  <link rel="stylesheet" href="../src/css/style_template.css">
+  <link rel="stylesheet" href="../src/css/style_hobbies.css">
+>>>>>>> 68c704f38376ed31843e4c4d23abf18655cb472e
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
   <link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&display=swap" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Poppins:wght@200&display=swap" rel="stylesheet">
@@ -22,7 +32,15 @@
         <div class="image-bg"> 
             
             <!--Intégrer ici une image de bg avec <img src.... /> -->
+<<<<<<< HEAD
+<<<<<<< HEAD
+            <img class="imageg" src= "./images/johnbg.jpg"/>
+=======
             <img class="imageg" src= "./medias/johnbg.jpg"/>
+>>>>>>> 68c704f38376ed31843e4c4d23abf18655cb472e
+=======
+            <img class="imageg" src= "./medias/johnbg.jpg"/>
+>>>>>>> 68c704f38376ed31843e4c4d23abf18655cb472e
         </div>
 
     </div>
@@ -33,6 +51,20 @@
         <div>    
             <h1 class='hobbies'>My Hobbies</h1>
 
+<<<<<<< HEAD
+<<<<<<< HEAD
+            <img class='giffy' src="images/wickdog.gif" >   
+            <h2>My Dog</h2>
+                <p>My dog, my only friend almost more loyal than my caliber .45APC. I love it.</p>
+  
+            <img class='giffy' src="images/johnasr.gif" >    
+            <h2>My Guns</h2>
+                <p>My weapons, I choose them as well as the entrée, my dishes and my dessert at the restaurant.</p>
+        
+            <img class='giffy' src="images/johnpolocrosse.gif" >
+=======
+=======
+>>>>>>> 68c704f38376ed31843e4c4d23abf18655cb472e
             <img class='giffy' src="./images/gifhobbies/wickdog.gif" >   
             <h2>My Dog</h2>
                 <p>My dog, my only friend almost more loyal than my caliber .45APC. I love it.</p>
@@ -42,6 +74,10 @@
                 <p>My weapons, I choose them as well as the entrée, my dishes and my dessert at the restaurant.</p>
         
             <img class='giffy' src="./images/gifhobbies/johnpolocrosse.gif" >
+<<<<<<< HEAD
+>>>>>>> 68c704f38376ed31843e4c4d23abf18655cb472e
+=======
+>>>>>>> 68c704f38376ed31843e4c4d23abf18655cb472e
             <h2>The Polo-Cross</h2>
                 <p>I know this is not common, however none of my hobbies are.</p>
         </div>
